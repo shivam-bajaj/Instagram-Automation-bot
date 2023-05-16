b10dfb5ca86348f2bace129c5b5d7c72f909f577
Shivam Bajaj <bajjuoo7s@gmail.com>
2023-05-16 23:11:24 +0530

    insta bot

--- insta_bot.py
+++ insta_bot.py
@@ -1,106 +1,156 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome  import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
-class Insta:
-    def __init__(self,username, password):
+'''
+
+4. Explicit Wait
+5. multi-user
+6. Excpetion Handling
+
+'''
+
+'''
+Can also scrap data from public or following accounts by adding parameter
+'''
+
+
+class InstaBot:
+    def __init__(self, username, password):
         self.username = username
         self.password = password
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.sign_in()
 
     def sign_in(self):
-        
-        '''
-        webdriver will wait for a page to load by default.
-          It does not wait for loading inside frames or for ajax requests. It means when you use .get('url'),
-        your browser will wait until the page is completely loaded and then go to the next command in the code. But when you are posting an ajax request,
-          webdriver does not wait and it's your responsibility to wait an appropriate amount of time for the page or a part of page to load; 
-          so there is a module named expected_conditions.
-        '''
-
 
         self.driver.get('https://instagram.com')
         sleep(2)
 
         # self.browser.implicitly_wait(10) # seconds
 
-
         # useranme and password input
-        self.driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(self.username)
-        self.driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(self.password)
-        
+        try:
+            self.driver.find_element(
+                By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(self.username)
+            self.driver.find_element(
+                By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(self.password)
+        except:
+            print("Error finding login Form")
         # Log in Button
-        self.driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[3]/button').click()
-        sleep(5)
-        self.driver.get('https://instagram.com/'+self.username+'/')
-        sleep(5)
-        self.get_followwers()
+        try:
+            self.driver.find_element(
+                By.XPATH, '/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button').click()
+            sleep(5)
+            self.driver.get('https://instagram.com/'+self.username+'/')
+            sleep(5)
+        except:
+            print('Problem with sign in...')
 
     def log_out(self):
-        
+
         self.driver.get('https://instagram.com/'+self.username+'/')
         sleep(2)
-        self.driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/div[2]/button').click()
+        self.driver.find_element(
+            By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/div[2]/button').click()
         sleep(2)
-        self.driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div/button[6]').click()
+        self.driver.find_element(
+            By.XPATH, '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div/button[6]').click()
         sleep(10)
 
-    def get_followwers(self):
+    def get_followers(self):
 
         self.driver.get('https://instagram.com/'+self.username+'/followers/')
         sleep(5)
-        last_ht,ht =0,1
-        try:                                            
-          scrollbox= self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]')
+        last_ht, ht = 0, 1
+        try:
+            scrollbox = self.driver.find_element_by_xpath(
+                '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]')
         except:
-            print('niche wala') 
-            scrollbox = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]')
-        while last_ht!=ht:
-            last_ht=ht
+            print('cant find scroll box')
+        while last_ht != ht:
+            last_ht = ht
             sleep(1)
-            ht= self.driver.execute_script("""
+            ht = self.driver.execute_script("""
                 arguments[0].scrollTo(0, arguments[0].scrollHeight);
                 return arguments[0].scrollHeight;
-                """,scrollbox)
+                """, scrollbox)
             links = scrollbox.find_elements_by_tag_name('a')
 
-        names=[]
+        names = []
         names = [name.text for name in links if name.text != '']
         print(names)
         print(len(names))
-        #close box
         sleep(2)
-        self.get_follwing()                                            
-        
-    def get_follwing(self):
+        return names
+
+    def get_following(self):
         self.driver.get('https://instagram.com/'+self.username+'/following/')
         sleep(5)
-        last_ht,ht =0,1
-        scrollbox = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]')
-        while last_ht!=ht:
-            last_ht=ht
+        last_ht, ht = 0, 1
+        scrollbox = self.driver.find_element_by_xpath(
+            '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]')
+        while last_ht != ht:
+            last_ht = ht
             sleep(1)
-            ht= self.driver.execute_script("""
+            ht = self.driver.execute_script("""
                 arguments[0].scrollTo(0, arguments[0].scrollHeight);
                 return arguments[0].scrollHeight;
-                """,scrollbox)
+                """, scrollbox)
             links = scrollbox.find_elements_by_tag_name('a')
 
-        names=[]
+        names = []
         names = [name.text for name in links if name.text != '']
         print(names)
         print(len(names))
-        #close box
         sleep(2)
+        return names
 
-        self.log_out()
+    def get_followers_info(self):
+
+        followers = self.get_followers()
+        sleep(2)
+        following = self.get_following()
+        not_following_back = [
+            user for user in following if user not in followers]
+        fans = [user for user in followers if user not in following]
+
+        print(not_following_back)
+        print(fans)
+
+        # remove verified signs
+        b = '\nVerified'
+        for i in range(0, len(not_following_back)):
+            if b in not_following_back[i]:
+                not_following_back[i] = not_following_back[i].replace(b, '')
+
+        return not_following_back, fans
+
+    def unfollow_user(self, user):
+
+        sleep(2)
+        self.driver.get('https://instagram.com/'+user+'/')
+        sleep(2)
+        try:
+            check_following = self.driver.find_element(
+                By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/div[1]/div/div[1]/button/div/div[1]')
+            if check_following.text == 'Following':
+                check_following.click()
+                sleep(2)
+                self.driver.find_element(
+                    By.XPATH, '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div/div[8]/div[1]').click()
+                sleep(2)
+            else:
+                print('Unable to unfollow')
+        except Exception as e:
+            print(e)
 
 
-
-        
-
-
-
-Insta(username,password)
+# username and password
+a = InstaBot('', '')
+not_following_back, fans = a.get_followers_info()
+print(not_following_back)
+for user in not_following_back:
+    a.unfollow_user(user)
+a.log_out()
